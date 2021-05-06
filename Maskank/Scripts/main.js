784fce847455f08a6aca6abb2a01fa1509cd7392
Mirna Mostafa <83687271+MirnaMostafa440@users.noreply.github.com>
2021-05-06 03:01:57 +0200

    Add files via upload

---
+++ Maskank/Scripts/main.js
@@ -0,0 +1,25 @@
+﻿
+//Pre Loader
+$(window).on('load', function () {
+    $(".loader-content").fadeOut(1000);
+    new WOW().init();
+})
+
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover({
+        placement: 'top',
+        html: true,
+        title: 'Noor Capital <a href="#" class="close" data-dismiss="alert">&times;</a>',
+    });
+    $(document).on("click", ".popover .close", function () {
+        $(this).parents(".popover").popover('hide');
+    });
+});
+// Mean Menu
+jQuery('.mean-menu').meanmenu({
+    meanScreenWidth: "991"
+});
+
+
+
+
